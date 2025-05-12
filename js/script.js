768e7c6547c2e9133d182a5097c185b8ebc90302
Stanislav Nemchenko <stas.buka@gmail.com>
2025-05-12 11:52:26 +0300

    Added type casting to the guess

--- js/script.js
+++ js/script.js
@@ -5,24 +5,28 @@ function checkGuess() {
     attempts -= 1;
     const inputElement = document.getElementById("guess");
     const feedbackElement = document.getElementById("feedback");
-    let guess = inputElement.value;
+    let guess = Number(inputElement.value);
     while (attempts > 0) {
+        console.log(randomNumber);
         if (guess === randomNumber) {
             attempts = 0;
-            feedbackElement.innerHTML = "Congratulations! You guessed the number!";
+            feedbackElement.innerHTML =
+                "Congratulations! You guessed the number!";
             feedbackElement.style.color = "green";
             break;
         } else if (guess < randomNumber) {
-            feedbackElement.innerHTML = "Too low! Try again. Attempts: " + `${attempts}`;
+            feedbackElement.innerHTML =
+                "Too low! Try again. Attempts: " + `${attempts}`;
             feedbackElement.style.color = "red";
             break;
         } else {
-            feedbackElement.innerHTML = "Too high! Try again. Attempts: " + `${attempts}`;
+            feedbackElement.innerHTML =
+                "Too high! Try again. Attempts: " + `${attempts}`;
             feedbackElement.style.color = "red";
             break;
         }
     }
-    if(attempts === 0 && guess !== randomNumber){
+    if (attempts === 0 && guess !== randomNumber) {
         feedbackElement.innerHTML = "Game over";
         feedbackElement.style.color = "red";
     }
